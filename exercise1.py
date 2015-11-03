229d72dc0a822542db9055d1b7936c47480e0aad
jeannemarie <jeannemarie.alfonso@mail.utoronto.ca>
2015-11-03 12:53:31 -0500

    Exercise 1 - edited codes

--- exercise1.py
+++ exercise1.py
@@ -12,6 +12,25 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
+
+def pig_latinify(word):
+    if len(word) > 0 and word.isalpha():
+        word = word.lower()
+        result = ""
+        vowel = "a,e,i,o,u"
+        if word[0] is vowel:
+            return result + yay
+        while word[0] is not vowel:
+            word[0] += (word[1:] + word[0])
+            if word[0] == vowel:
+                print result + "ay"
+        else:
+            return ""
+
+print pig_latinify("crapple")
+
+
+"""
 def pig_latinify(word):
     """
 
@@ -24,14 +43,20 @@ def pig_latinify(word):
     if len(word) > 0 and word.isalpha():
         first_letter = word[0]
         word = word.lower()
+        vowel = "a" or "e" or "i" or "o" or "u":
         result = []
-        if first_letter == "a" or "e" or "i" or "o" or "u":
+        if first_letter :
             result.append(word + "yay")
             return result
         elif first_letter != "a" or "e" or "i" or "o" or "u":
             result.append(word[1:len(word)] + first_letter + "ay")
             return result
-    else: return ""
 
+
+"""
+
+""" consonant word + ay"  change to while loop to accomodate words liek "scratch"
+    else:
+        return """
 
 
