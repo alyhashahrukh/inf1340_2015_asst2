bbde47e8b4d5ecb61fedac11d093a55327667ae0
jeannemarie <jeannemarie.alfonso@mail.utoronto.ca>
2015-11-04 10:21:23 -0500

    Exercise 3 - changed union content

--- exercise3.py
+++ exercise3.py
@@ -12,7 +12,6 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-
 GRADUATES = [["Number", "Surname", "Age"],
             [7274, "Robinson", 37],
             [7432, "O'Malley", 39],
@@ -24,28 +23,27 @@ MANAGERS = [["Number", "Surname", "Age"],
             [9824, "Darkes", 38]]
 
 #deep copy of GRADUATES[0]
-t1 = [] + GRADUATES[0]
-t1
+table1 = [] + GRADUATES[0]
+table1
 #deep copy of MANAGERS[0]
-t2 = [] + MANAGERS[0]
-t2
+table2 = [] + MANAGERS[0]
+table2
+
 
 """
-WHAT THEY ARE LOOKING FOR:
 NUMBER OF COLUMNS
 len(t1) == len(t2)
-
 NAMES OF COLUMNS
 str(t1) == str(t2)
-
 ORDER OF COLUMNS
-t1 == t2 ?????
+t1 == t2
 """
 
-def union(t1, t2):
+
+
+def union(table1, table2):
     """
     Perform the union set operation on tables, table1 and table2.
-
     :param t1: a table (a List of Lists)
     :param t2: a table (a List of Lists)
     :return: the resulting table union
@@ -54,16 +52,22 @@ def union(t1, t2):
     """
 
     table_union = []
-    for unify in t1 and t2:
-        unify = t1.union(t2)
-        table_union.append(unify)
-    return table_union
+    while len(table1) == len(table2) and str(table1) == str(table2) and table1 == table2:
+        return True
+        table_union = [] + (table1 + table2)
+        print table_union
+
+    """
+    we want this output
+    #union
+    t3 = t1 + t2
+    print t3
+    """
 
 
-def intersection(t1, t2):
+def intersection(table1, table2):
     """
     Perform the intersection set operation on tables, table1 and table2.
-
     :param t1: a table (a List of Lists)
     :param t2: a table (a List of Lists)
     :return: the resulting table intersection
@@ -74,13 +78,17 @@ def intersection(t1, t2):
     for intersect in t1 and t2:
         intersect = t1.intersection(t2)
         table_intersection.append(intersect)
-    return table_intersection
+        return table_intersection
+    """
+    we want this output
+    #intersection
+    t4 = t1 and t2
+    print t4
+    """
 
-
-def difference(t1, t2):
+def difference(table1, table2):
     """
     Perform the difference set operation on tables, table1 and table2.
-
     :param t1: a table (a List of Lists)
     :param t2: a table (a List of Lists)
     :return: the resulting table difference
@@ -91,7 +99,7 @@ def difference(t1, t2):
     for differ in t1 and t2:
         differ = t1.difference(t2)
         table_difference.append(differ)
-    return table_difference
+        return table_difference
 
 
 
